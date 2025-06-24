494f7d361c0dcfb6bf3f65e1bdcd5741d129eaa0
callmeAngga <wongfromindo@gmail.com>
2025-06-24 15:07:41 +0700

    fix error saat coba deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router'
 import AppLayout from './layout/AppLayout'
 import StatisticPage from './pages/StatisticPage'
 import CoursePage from './pages/CoursePage'
-import ModulePage from './pages/ModulePage'
-import ContentPage from './pages/ContentPage'
 import ProfilePage from './pages/ProfllePage'
 import AddAdminPage from './pages/AddAdminPage'
 import LoginPage from './pages/LoginPage'
@@ -19,8 +17,6 @@ function App() {
           <Route path="/" element={<AppLayout />}>
             <Route path="/dashboard" element={<StatisticPage />}/>
             <Route path="/courses" element={<CoursePage />} />
-            <Route path="/modules" element={<ModulePage />} />
-            <Route path="/contents" element={<ContentPage />} />
             <Route path="/admin" element={<AddAdminPage />} />
             <Route path="/profile" element={<ProfilePage />} />
           </Route>
